0c84cadded3c6817d51ae8fd5870facf9fa26e76
Brandon Winger-Air <brandonwinger-air@outlook.com>
2019-04-23 06:55:00 -0400

    Add client folder back to git repo

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import List from './components/List';
+import ItemModal from './components/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar/>
+          <Container>
+            <ItemModal/>
+            <List/>
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
